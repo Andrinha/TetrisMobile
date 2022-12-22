6738131937b3c1bc25d7e881409ff2e273eac4c3
Kalachev <Kalachev@o-code.ru>
2022-12-22 11:45:36 +0400

    fix shape duplicate check

--- app/src/main/java/com/fit/tetris/ui/blockeditor/BlockEditorActivity.kt
+++ app/src/main/java/com/fit/tetris/ui/blockeditor/BlockEditorActivity.kt
@@ -13,11 +13,13 @@ import androidx.core.view.get
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.fit.tetris.R
-import com.fit.tetris.data.Action
 import com.fit.tetris.data.shape.Shape
 import com.fit.tetris.data.shape.ShapeDatabase
 import com.fit.tetris.databinding.ActivityBlockEditorBinding
+import com.fit.tetris.utils.BaseShapes
 import com.fit.tetris.utils.toBooleanArray
+import java.lang.Integer.max
+import java.lang.Integer.min
 import java.util.*
 
 class BlockEditorActivity : AppCompatActivity() {
@@ -83,7 +85,7 @@ class BlockEditorActivity : AppCompatActivity() {
                 binding.textIsConnected.setTextColor(ContextCompat.getColor(this, R.color.color_10))
                 isShapeGood = false
             }
-            if (checkForDuplicate(tiles, viewModel.shapes)) {
+            if (checkForDuplicate(truncate(tiles), viewModel.shapes.map { truncate(it) })) {
                 binding.textIsStored.text = "Фигура новая"
                 binding.textIsStored.setTextColor(
                     ContextCompat.getColor(
@@ -101,12 +103,15 @@ class BlockEditorActivity : AppCompatActivity() {
                 )
                 isShapeGood = false
             }
-
+            binding.textValue.text = count(tiles).toString()
             binding.buttonSave.isEnabled = isShapeGood
+
         }
 
         viewModel.shapesData.observe(this) { shapes ->
-            viewModel.shapes = shapes.map { it.tiles.toBooleanArray() }.toMutableList()
+            val based = BaseShapes().list.map { it.toBooleanArray() }
+            val saved = shapes.map { it.tiles.toBooleanArray() }
+            viewModel.shapes = (based + saved).toMutableList()
         }
     }
 
@@ -130,38 +135,129 @@ class BlockEditorActivity : AppCompatActivity() {
             return true
 
         shapeList.forEach { shape ->
-            var contains = true
-            repeat(4) { j ->
-                repeat(4) { i ->
-                    if (shape[i][j] != tiles[i][j]) {
-                        contains = false
+            if (shape.size == tiles.size) {
+                var rotated = shape
+                repeat(4) {
+                    rotated = rotateCCW(rotated)
+                    var contains = true
+                    repeat(tiles.size) { j ->
+                        repeat(tiles.size) { i ->
+                            if (rotated[i][j] != tiles[i][j]) {
+                                contains = false
+                            }
+                        }
+                    }
+                    if (contains) {
+                        return false
                     }
                 }
-            }
-            if (contains) {
-                return false
             }
         }
         return true
     }
 
-    private fun move(tiles: Array<BooleanArray>, action: Action) {
-        val temp = tiles.clone()
-        when (action) {
-            Action.LEFT -> {
-                repeat(3) {
-                    temp[it] = tiles[it + 1]
-                }
-            }
-            Action.RIGHT -> {
-                repeat(3) {
-                    temp[it] = tiles[it - 1]
-                }
-            }
-            else -> {
+    private fun getSize(tiles: Array<BooleanArray>): Int {
+        if (count(tiles) == 0)
+            return 0
 
+        var genMinHeight = Int.MAX_VALUE
+        var genMaxHeight = Int.MIN_VALUE
+
+        var genMinWidth = Int.MAX_VALUE
+        var genMaxWidth = Int.MIN_VALUE
+        repeat(tiles.size) { j ->
+            var minHeight = Int.MAX_VALUE
+            var maxHeight = Int.MIN_VALUE
+            var isHeightFirst = true
+
+            var minWidth = Int.MAX_VALUE
+            var maxWidth = Int.MIN_VALUE
+            var isWidthFirst = true
+            repeat(tiles.size) { i ->
+                if (tiles[i][j]) {
+                    if (isHeightFirst) {
+                        minHeight = i
+                        isHeightFirst = false
+                    }
+                    maxHeight = i
+                }
+
+                if (tiles[i][j]) {
+                    if (isWidthFirst) {
+                        minWidth = j
+                        isWidthFirst = false
+                    }
+                    maxWidth = j
+                }
+            }
+            genMinHeight = min(genMinHeight, minHeight)
+            genMaxHeight = max(genMaxHeight, maxHeight)
+
+            genMinWidth = min(genMinWidth, minWidth)
+            genMaxWidth = max(genMaxWidth, maxWidth)
+        }
+        return max(genMaxHeight - genMinHeight, genMaxWidth - genMinWidth) + 1
+    }
+
+    private fun truncate(_tiles: Array<BooleanArray>): Array<BooleanArray> {
+        when (val size = getSize(_tiles)) {
+            4 -> return _tiles
+            else -> {
+                val tiles = Array(size) { BooleanArray(size) }
+                var maxSize = Int.MIN_VALUE
+                var offsetI = 0
+                var offsetJ = 0
+                repeat(4 - size + 1) { j ->
+                    repeat(4 - size + 1) { i ->
+
+                        repeat(size) { y ->
+                            repeat( size) { x ->
+                                tiles[x][y] = _tiles[x + i][y + j]
+                            }
+                        }
+                        val locSize = count(tiles)
+                        if (locSize > maxSize) {
+                            maxSize = locSize
+                            offsetI = i
+                            offsetJ = j
+                        }
+                    }
+                }
+                repeat(size) { y ->
+                    repeat( size) { x ->
+                        tiles[x][y] = _tiles[x + offsetI][y + offsetJ]
+                    }
+                }
+
+                return tiles
             }
         }
+    }
+
+    private fun count(tiles: Array<BooleanArray>): Int {
+        val size = tiles.size
+        var sum = 0
+        repeat(size) { j ->
+            repeat(size) { i ->
+                if (tiles[i][j]) {
+                    sum++
+                }
+            }
+        }
+        return sum
+    }
+
+    private fun rotateCCW(tiles: Array<BooleanArray>): Array<BooleanArray> {
+        for (i in 0 until tiles.size / 2) {
+            for (j in i until tiles.size - i - 1) {
+                val temp: Boolean = tiles[i][j]
+                tiles[i][j] = tiles[tiles.size - 1 - j][i]
+                tiles[tiles.size - 1 - j][i] = tiles[tiles.size - 1 - i][tiles.size - 1 - j]
+                tiles[tiles.size - 1 - i][tiles.size - 1 - j] = tiles[j][tiles.size - 1 - i]
+                tiles[j][tiles.size - 1 - i] = temp
+            }
+        }
+        return tiles
     }
 
     private fun countNeighbors(tiles: Array<BooleanArray>, i: Int, j: Int): Int {
